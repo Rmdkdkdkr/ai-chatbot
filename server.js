95ebc738ec6650d54b4d2f5b744a92a614bfbf22
Rmdkdkdkr <2sangyoon17@gmail.com>
2026-06-30 16:05:41 +0900

    fix: static path for deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const OpenAI = require("openai").default;
 const path = require("path");
 const app = express();
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/chat", (req, res) => res.sendFile(path.join(__dirname, "public", "chat.html")));
 
